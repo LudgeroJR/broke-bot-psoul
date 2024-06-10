5dcd39315d3365c418748123b35fa31e96a6cef5
Ludgero Junior <ludgerojunior87@gmail.com>
2024-06-10 09:53:19 -0300

    Disable reset command

--- src/commands/game/game-reset.js
+++ src/commands/game/game-reset.js
@@ -73,7 +73,7 @@ module.exports = {
   devOnly: true,
   // memberOnly: true,
   // testOnly: true,
-  // deleted: Boolean,
+  deleted: Boolean,
 
   callback: async (client, interaction) => {
     // Criando uma nova lista de pokemon
